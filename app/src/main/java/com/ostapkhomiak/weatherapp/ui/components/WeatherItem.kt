e500b20862ce895ff7a48d43ac31e7dec3a32b24
Ostap Khomiak <ostapkhomiak2023@gmail.com>
2025-12-03 14:15:55 +0100

    ui: improved CurrentScreen looks

--- app/src/main/java/com/ostapkhomiak/weatherapp/ui/components/WeatherItem.kt
+++ app/src/main/java/com/ostapkhomiak/weatherapp/ui/components/WeatherItem.kt
@@ -1,13 +1,18 @@
 package com.ostapkhomiak.weatherapp.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,30 +22,39 @@ import com.ostapkhomiak.weatherapp.domain.model.HourWeather
 
 @Composable
 fun WeatherItem(item: HourWeather) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 12.dp, horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(8.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color(0x1A000000))  // 10% black
     ) {
-        Text(
-            text = item.time,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium,
-            modifier = Modifier.weight(1f)
-        )
-        AsyncImage(
-            model = item.iconUrl,
-            contentDescription = null,
-            modifier = Modifier.size(80.dp)
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = item.time,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "${item.temp}°C",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 20.dp)
+            )
 
+            AsyncImage(
+                model = item.iconUrl,
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+            )
 
-        Text(
-            text = "${item.temp}°C",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 20.dp)
-        )
+        }
     }
+
 }
--- app/src/main/java/com/ostapkhomiak/weatherapp/ui/screens/current/CurrentScreen.kt
+++ app/src/main/java/com/ostapkhomiak/weatherapp/ui/screens/current/CurrentScreen.kt
@@ -1,20 +1,27 @@
 package com.ostapkhomiak.weatherapp.ui.screens.current
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -67,31 +74,64 @@ fun WeatherContent(weather: Weather, cityName: String?) {
 
         Text(
             text = cityName ?: "",
-            fontSize = 32.sp
+            fontSize = 32.sp,
+            fontWeight = FontWeight.SemiBold
         )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        AsyncImage(
-            model = weather.iconUrl,
-            contentDescription = null,
-            modifier = Modifier.size(160.dp)
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color(0x33000000)) // 20% black
+                .padding(vertical = 32.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                AsyncImage(
+                    model = weather.iconUrl,
+                    contentDescription = null,
+                    modifier = Modifier.size(150.dp)
+                )
 
-        Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(12.dp))
 
-        Text(
-            text = "${weather.temperature}°C",
-            fontSize = 48.sp
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(weather.hours) { hour ->
-                WeatherItem(hour)
+                Text(
+                    text = "${weather.temperature}°C",
+                    fontSize = 64.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Hourly Forecast",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.align(Alignment.Start).padding(horizontal = 24.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color(0x1A000000)) // 10% black
+
+        ) {
+            LazyColumn {
+                items(weather.hours) { hour ->
+                    WeatherItem(hour)
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
     }
 }
 
